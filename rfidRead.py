dc9d5a3edfd1cdf210b004960f0de891c309b677
Camerontaylorr <46680421+Camerontaylorr@users.noreply.github.com>
2023-03-30 21:41:21 +0100

    Add files via upload

--- rfidRead.py
+++ rfidRead.py
@@ -1,18 +1,23 @@
-         #!/usr/bin/env python
+#!/usr/bin/env python
 
+#import libaries and modules
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import pyttsx3
 engine = pyttsx3.init()
 import RPi.GPIO as GPIO
 
+#disable any gpio warnings
 GPIO.setwarnings(False)
+
+#set up the rfid reader
 reader = SimpleMFRC522()
 
 try:
-        id, text = reader.read()
+        #id, text = reader.read()
+
+        #read the id of the tag
         id = reader.read_id()
-        print(id)
-        print(id)
+        print(id) #print the id
 finally:
-        GPIO.cleanup()
+        GPIO.cleanup() #clean up the gpio pins
